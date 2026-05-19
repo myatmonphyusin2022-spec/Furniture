94b94e9121459b5a62c85e9e737da33bfb1f8e7c
Myat Mon Phyu Sin <myatmonphyusin2022@gmail.com>
2026-05-19 18:09:07 +0630

    Updated Product

--- Furniture/src/pages/products/Product.tsx
+++ Furniture/src/pages/products/Product.tsx
@@ -5,9 +5,9 @@ import ProductFilter from "@/components/products/ProductFilter";
 import Pagination from "@/components/products/Pagination";
 function Product() {
   return (
-    <div>
-      <section className="">
-        <section className="">
+    <div className="container mx-auto">
+      <section className="flex flex-col lg:flex-row">
+        <section className="my-8 ml-4 w-full lg:ml-0 lg:w-1/5">
           <ProductFilter
             title="Furniture made by"
             filterlist={filterList.categories}
@@ -17,9 +17,9 @@ function Product() {
             filterlist={filterList.types}
           />
         </section>
-        <section className="">
-          <h1 className="">All Products</h1>
-          <div className="">
+        <section className="my-8 ml-4 w-full lg:ml-0 lg:w-4/5">
+          <h1 className="text-2xl font-bold">All Products</h1>
+          <div className="mb-12 grid grid-cols-1 gap-6 gap-y-12 px-4 md:grid-cols-2 md:px-0 lg:grid-cols-3">
             {products.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
